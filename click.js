162af4469635be29a2d8f3ca6fdaab49c0b870ad
Chun Hao Wang <chunhao137@gmail.com>
2014-07-20 07:39:10 -0700

    Initial Commit

---
+++ click.js
@@ -0,0 +1,483 @@
+    
+	$(document).ready(function (){  
+
+  
+
+    var par1 = $('p.messages1');
+    $(par1).hide();   
+
+     var par2 = $('p.messages2');
+    $(par2).hide();
+
+     var par3 = $('p.messages3');
+    $(par3).hide();  
+
+     var par4 = $('p.messages4');
+    $(par4).hide();  
+
+      var par5 = $('p.messages5');
+    $(par5).hide();
+
+    var par6 = $('p.messages6');
+    $(par6).hide();
+
+    var par7 = $('p.messages7');
+    $(par7).hide();
+
+    var par8 = $('p.messages8');
+    $(par8).hide();
+
+    var par9 = $('p.messages9');
+    $(par9).hide();
+
+    var par10 = $('p.messages10');
+    $(par10).hide();
+
+    var par11 = $('p.messages11');
+    $(par11).hide();
+
+    var par12 = $('p.messages12');
+    $(par12).hide();
+
+    var par13 = $('p.messages13');
+    $(par13).hide();
+
+    var par14 = $('p.messages14');
+    $(par14).hide();
+
+    var par15 = $('p.messages15');
+    $(par15).hide();
+
+    var par16 = $('p.messages16');
+    $(par16).hide();
+
+    var par17 = $('p.messages17');
+    $(par17).hide();
+
+    var par18 = $('p.messages18');
+    $(par18).hide();
+
+    var par19 = $('p.messages19');
+    $(par19).hide();
+
+    var par20 = $('p.messages20');
+    $(par20).hide();
+    
+    var par21 = $('p.messages21');
+    $(par21).hide();
+
+    var par22 = $('p.messages22');
+    $(par22).hide();
+
+    var par23 = $('p.messages23');
+    $(par23).hide();
+
+    var par24 = $('p.messages24');
+    $(par24).hide();
+
+    var par25 = $('p.messages25');
+    $(par25).hide();
+
+    var parjapp1 = $('p.japp1');
+    $(parjapp1).hide();
+
+     var parjapp2 = $('p.japp2');
+    $(parjapp2).hide();
+
+     var parjapp3 = $('p.japp3');
+    $(parjapp3).hide();
+
+     var parjapp4 = $('p.japp4');
+    $(parjapp4).hide();
+
+    var parjapp5 = $('p.japp5');
+    $(parjapp5).hide();
+
+    var parjapp6 = $('p.japp6');
+    $(parjapp6).hide();
+
+    var html51 = $('p.canvas1');
+    $(html51).hide();
+
+     var parap1 = $('p.apmessage1');
+    $(parap1).hide();
+    
+     
+     $('#newanswer1').click(function () {
+          $(par1).slideToggle('slow');
+          $(this).text(function(i, text){
+          return text === "Answer" ? "Hide" : "Answer";
+      })
+
+    })
+
+      $('#newanswer2').click(function () {
+        
+          $(par2).slideToggle('slow');
+          $(this).text(function(i, text){
+          return text === "Answer" ? "Hide" : "Answer";
+          })
+      })
+
+      $('#newanswer3').click(function () {
+         $(par3).slideToggle('slow');
+          $(this).text(function(i, text){
+          return text === "Answer" ? "Hide" : "Answer";
+      
+        })
+
+      })
+
+       $('#newanswer4').click(function () {
+         $(par4).slideToggle('slow');
+          $(this).text(function(i, text){
+          return text === "Answer" ? "Hide" : "Answer";
+      
+        })
+
+      })
+
+       $('#newanswer5').click(function () {
+         $(par5).slideToggle('slow');
+          $(this).text(function(i, text){
+          return text === "Answer" ? "Hide" : "Answer";
+      
+        })
+
+      })
+
+      $('#newanswer6').click(function () {
+         $(par6).slideToggle('slow');
+          $(this).text(function(i, text){
+          return text === "Answer" ? "Hide" : "Answer";
+      
+        })
+    })
+
+        $('#newanswer7').click(function () {
+         $(par7).slideToggle('slow');
+          $(this).text(function(i, text){
+          return text === "Answer" ? "Hide" : "Answer";
+      
+        })
+
+      })
+
+        $('#newanswer8').click(function () {
+         $(par8).slideToggle('slow');
+          $(this).text(function(i, text){
+          return text === "Answer" ? "Hide" : "Answer";
+      
+        })
+
+      })
+
+
+        $('#newanswer9').click(function () {
+         $(par9).slideToggle('slow');
+          $(this).text(function(i, text){
+          return text === "Answer" ? "Hide" : "Answer";
+      
+        })
+
+      })
+
+         $('#newanswer10').click(function () {
+         $(par10).slideToggle('slow');
+          $(this).text(function(i, text){
+          return text === "Answer" ? "Hide" : "Answer";
+      
+        })
+
+      })
+
+
+         $('#newanswer11').click(function () {
+         $(par11).slideToggle('slow');
+         $(this).text(function(i, text){
+          return text === "Answer" ? "Hide" : "Answer";
+      
+        })
+
+      })
+         $('#newanswer12').click(function () {
+         $(par12).slideToggle('slow');
+         $(this).text(function(i, text){
+          return text === "Answer" ? "Hide" : "Answer";
+      
+        })
+
+      })
+
+         $('#newanswer13').click(function () {
+         $(par13).slideToggle('slow');
+         $(this).text(function(i, text){
+          return text === "Answer" ? "Hide" : "Answer";
+      
+        })
+
+      })
+
+         $('#newanswer14').click(function () {
+         $(par14).slideToggle('slow');
+         $(this).text(function(i, text){
+          return text === "Answer" ? "Hide" : "Answer";
+      
+        })
+
+      })
+
+        $('#newanswer15').click(function () {
+        $(par15).slideToggle('slow');
+        $(this).text(function(i, text){
+        return text === "Answer" ? "Hide" : "Answer";
+      
+        })
+
+      })
+
+
+        $('#newanswer16').click(function () {
+        $(par16).slideToggle('slow');
+        $(this).text(function(i, text){
+        return text === "Answer" ? "Hide" : "Answer";
+      
+        })
+
+      })
+        $('#newanswer17').click(function () {
+        $(par17).slideToggle('slow');
+        $(this).text(function(i, text){
+        return text === "Answer" ? "Hide" : "Answer";
+      
+        })
+
+      })
+
+       
+        $('#newanswer18').click(function () {
+        $(par18).slideToggle('slow');
+        $(this).text(function(i, text){
+        return text === "Answer" ? "Hide" : "Answer";
+      
+        })
+
+      })
+
+
+        $('#newanswer19').click(function () {
+        $(par19).slideToggle('slow');
+        $(this).text(function(i, text){
+        return text === "Answer" ? "Hide" : "Answer";
+      
+        })
+
+      })
+
+        $('#newanswer20').click(function () {
+        $(par20).slideToggle('slow');
+        $(this).text(function(i, text){
+        return text === "Answer" ? "Hide" : "Answer";
+      
+        })
+
+      })
+
+        $('#newanswer21').click(function () {
+        $(par21).slideToggle('slow');
+        $(this).text(function(i, text){
+        return text === "Answer" ? "Hide" : "Answer";
+      
+        })
+
+      })
+
+         $('#newanswer22').click(function () {
+        $(par22).slideToggle('slow');
+        $(this).text(function(i, text){
+        return text === "Answer" ? "Hide" : "Answer";
+      
+        })
+
+      })
+
+         $('#newanswer23').click(function () {
+        $(par23).slideToggle('slow');
+        $(this).text(function(i, text){
+        return text === "Answer" ? "Hide" : "Answer";
+      
+        })
+
+      })
+
+          $('#newanswer24').click(function () {
+        $(par24).slideToggle('slow');
+        $(this).text(function(i, text){
+        return text === "Answer" ? "Hide" : "Answer";
+      
+        })
+
+      })
+
+        $('#newanswer25').click(function () {
+        $(par25).slideToggle('slow');
+        $(this).text(function(i, text){
+        return text === "Answer" ? "Hide" : "Answer";
+      
+        })
+
+      })
+
+          $('#newap1').click(function () {
+        $(parap1).slideToggle('slow');
+        $(this).text(function(i, text){
+        return text === "Answer" ? "Hide" : "Answer";
+      
+        })
+
+      })
+
+          $('#jappbutton1').click(function () {
+        $(parjapp1).slideToggle('slow');
+        $(this).text(function(i, text){
+        return text === "Answer" ? "Hide" : "Answer";
+      
+        })
+
+      })
+
+        $('#jappbutton2').click(function () {
+        $(parjapp2).slideToggle('slow');
+        $(this).text(function(i, text){
+        return text === "Answer" ? "Hide" : "Answer";
+      
+        })
+
+      })
+
+        $('#jappbutton3').click(function () {
+        $(parjapp3).slideToggle('slow');
+        $(this).text(function(i, text){
+        return text === "Answer" ? "Hide" : "Answer";
+      
+        })
+
+      })
+
+         $('#jappbutton4').click(function () {
+        $(parjapp4).slideToggle('slow');
+        $(this).text(function(i, text){
+        return text === "Answer" ? "Hide" : "Answer";
+      
+        })
+
+      })
+
+        $('#jappbutton5').click(function () {
+        $(parjapp5).slideToggle('slow');
+        $(this).text(function(i, text){
+        return text === "Answer" ? "Hide" : "Answer";
+      
+        })
+
+      })
+
+        $('#jappbutton6').click(function () {
+        $(parjapp6).slideToggle('slow');
+        $(this).text(function(i, text){
+        return text === "Answer" ? "Hide" : "Answer";
+      
+        })
+
+      })
+
+        $('#canvasbutton1').click(function () {
+        $(html51).slideToggle('slow');
+        $(this).text(function(i, text){
+        return text === "Answer" ? "Hide" : "Answer";
+      
+        })
+
+      })
+
+        $('#circle1').click(function(){
+        $(this).hide("explode",1000); 
+          
+        })
+
+        $('#circle2').click(function(){
+        $(this).fadeOut("slow"); 
+          
+        })
+       
+       
+          $('.square1').click(function(){
+          $('div .square1').animate({left:'870px'},1500); 
+
+        })
+       
+        $(".yelp").hover(function(){
+        $(this).filter(':not(:animated)').animate({ width: "200px" });
+        }, function() {
+        $(this).animate({ width: "100px" });
+        });
+
+        $(".yelp").click(function(){
+
+        window.location.href = "http://www.yelp.com/";
+
+        })
+
+        $(".cnn").hover(function(){
+        $(this).filter(':not(:animated)').animate({ width: "200px" });
+        }, function() {
+        $(this).animate({ width: "100px" });
+        });
+
+        $(".cnn").click(function(){
+
+        window.location.href = "http://www.cnn.com";
+
+        })
+
+        $(".google").hover(function(){
+        $(this).filter(':not(:animated)').animate({ width: "200px" });
+        }, function() {
+        $(this).animate({ width: "100px" });
+        });
+
+        $(".google").click(function(){
+
+        window.location.href = "https://www.google.com";
+
+        })
+          
+
+        $("#slideshow > div:gt(0)").hide();
+
+
+setInterval(function () {
+    $('#slideshow > div:first')
+        .fadeOut(2000)
+        .next()
+        .fadeIn(2000)
+        .end()
+        .appendTo('#slideshow');
+}, 3000);
+
+
+
+
+
+
+
+     })
+
+
+
+
+    
+
+
+
+    
